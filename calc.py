e5adab9ffc442942247517df571dcb001f14ea1e
Bhuvanasree <bhuvanasree.m1999@gmail.com>
2024-02-08 20:50:21 +0530

    adding two numbers

--- calc.py
+++ calc.py
@@ -1,1 +1,10 @@
-a=20
+# Prompting the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+result = num1 + num2
+
+# Printing the result
+print("The sum of", num1, "and", num2, "is:", result)
+
